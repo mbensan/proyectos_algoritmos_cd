497edf319274062809466a4fa3ab76c788bddc3d
Matias Bensan A <mbensan@gmail.com>
2023-01-10 20:03:46 -0300

    Se añade funcion de combinaciones posibles

--- estructuras_datos/recursion.py
+++ estructuras_datos/recursion.py
@@ -50,6 +50,45 @@ def palabras_telef(num):
     return comb_1 + comb_2 + comb_3
 
 
+def hanoi(num_discos, origen='A', destino='C'):
+    assert isinstance(num_discos, int)
+    assert num_discos > 0
+
+    medio = 'ABC'.replace(origen, '').replace(destino, '')
+
+    if num_discos == 1:
+        return [f'1 -> {destino}']
+    
+    elif num_discos == 2:
+        return [f'1 -> {medio}', f'2 -> {destino}', f'1 -> {destino}']
+    
+    return hanoi(num_discos - 1, origen, medio) + [f'{num_discos} -> {destino}'] + hanoi(num_discos - 1, medio, destino)
+
+
+
+def uvas(bolsitas): # [20, 22, 1, 1, 4]
+    indices = set([i for i in range(len(bolsitas))])
+    intentos = [intentar(bolsitas, i, indices) for i in range(len(bolsitas))]
+    return max(intentos)
+
+def combinaciones(elementos):
+    if len(elementos) == 1:
+        return [[elementos[0]], []]
+    
+    primero = elementos[0]
+    elementos_cpy = list(elementos[1:])
+    
+    sin_primero = combinaciones(elementos_cpy)
+    con_primero = []
+    for elem in sin_primero:
+        elem.append(primero)
+        con_primero.append(elem)
+    
+    final = sin_primero + con_primero
+    return set(final)
+
+    
+
 
 import pdb; pdb.set_trace()
 
